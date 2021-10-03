984a80e2c850891b1509910009f6a92ae5a070de
rajmangukiya <rajmangukiya00@gmail.com>
2021-10-03 19:21:44 +0530

    commit

--- app.ts
+++ app.ts
@@ -29,7 +29,7 @@ app.get('/', (req: any, res: Response) => {
 createConnection(PORT === 5000 ? development : test)
   .then(() => {
     httpServer.listen(PORT, () => {
-      console.log(`Server is running on port: ${PORT}`);
+      console.log(`Server is running on port : ${PORT}`);
     });
   })
   .catch((e) => {
